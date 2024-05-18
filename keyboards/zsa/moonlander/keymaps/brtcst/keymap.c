7fd22ebf80e866a3f0dd075ecc233b9013a8a2bb
Bertrand CASTELLI <bertrand.castelli@gmail.com>
2024-05-18 17:14:48 +0200

    test

--- keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
+++ keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
@@ -95,10 +95,10 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     BP_DLR,  BP_DQUO, BP_LDAQ, BP_RDAQ, BP_LPRN, BP_RPRN, QK_LEAD,              BP_PERC, BP_AT,   BP_PLUS, BP_MINS, BP_SLSH, BP_ASTR, BP_EQL,
-    XXXXXXX, BP_B,    BP_EACU, BP_P,    BP_O,    BP_EGRV, XXXXXXX,              TD(D_22),BP_DCIR, BP_V,    BP_D,    BP_L,    BP_J,    BP_Z,
-    KC_CAPS, BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM, BP_W,                 BP_CCED, BP_C,    BP_T,    BP_S,    BP_R,    BP_N,    BP_M,
+    BP_W,    BP_B,    BP_EACU, BP_P,    BP_O,    BP_EGRV, XXXXXXX,              TD(D_22),BP_DCIR, BP_V,    BP_D,    BP_L,    BP_J,    BP_Z,
+    KC_CAPS, BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM, KC_TAB,               BP_CCED, BP_C,    BP_T,    BP_S,    BP_R,    BP_N,    BP_M,
     KC_LSFT, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,                                   BP_QUOT, BP_Q,    BP_G,    BP_H,    BP_F,    KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, TD(D_21),KC_TAB,         LGUI(BP_SCLN),        TD(D_2),          KC_BSPC, KC_DEL, KC_LALT, MO(4),   KC_RCTL,
+    KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, TD(D_21),         LGUI(BP_SCLN),        TD(D_2),          KC_BSPC, KC_DEL,  XXXXXXX, KC_LALT, KC_RCTL,
                                         KC_SPC,  SH_MON,  TD(D_1),              TD(D_3), MO(2),   KC_RALT
   ),
   [1] = LAYOUT_moonlander( //gaming
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  _______, _______, _______, _______, _______, BP_DCIR,              _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
-    KC_LCTL, _______, KC_LALT, _______, _______,          _______,              TD(D_4),          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______,              TD(D_4),          _______, _______, _______, _______, _______,
                                         _______, _______, _______,              _______, _______, _______
   ),
   [2] = LAYOUT_moonlander( //numpad
