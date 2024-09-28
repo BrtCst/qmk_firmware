658590a64fa86cbe5851b77c432c37dc464c9e3e
Bertrand CASTELLI <bertrand.castelli@gmail.com>
2024-09-28 18:38:11 +0200

    integrate brtcst keymap in 0.26.6

---
+++ keyboards/zsa/moonlander/keymaps/brtcst/rules.mk
@@ -0,0 +1,15 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUDIO_ENABLE = no
+MOUSEKEY_ENABLE = no
+ORYX_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+COMBO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+LEADER_ENABLE = yes
+SRC = matrix.c
+RGB_MATRIX_CUSTOM_USER = yes
