2c7941b5e61a2290399f7fcf0e3586aadf525d43
Bertrand CASTELLI <b@brtcst.net>
2025-08-23 00:04:31 +0200

    ménage

--- keyboards/zsa/moonlander/keymaps/brtcst/config.h
+++ keyboards/zsa/moonlander/keymaps/brtcst/config.h
@@ -10,14 +10,13 @@
 #define RGB_DISABLE_TIMEOUT 300000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define CAPS_LOCK_STATUS
-#define FIRMWARE_VERSION u8"VWLEe/9KbzN"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
-#define LAYER_STATE_8BIT
+//#define FIRMWARE_VERSION u8"VWLEe/9KbzN"
+//#define RAW_USAGE_PAGE 0xFF60
+//#define RAW_USAGE_ID 0x61
+//#define LAYER_STATE_8BIT
 //#define COMBO_COUNT 32
 
-#define RGB_MATRIX_STARTUP_SPD 60
+//#define RGB_MATRIX_STARTUP_SPD 60
 
 #define PERMISSIVE_HOLD_PER_KEY
 
@@ -34,5 +33,3 @@
 #define RGB_MATRIX_DEFAULT_SAT 0
 // pour avoir les indicator leds en custom
 #define MOONLANDER_USER_LEDS
-
-#undef CAPS_LOCK_STATUS
