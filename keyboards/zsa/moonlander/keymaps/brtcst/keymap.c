206eff87e347f90c34d83fea672e501000834e1d
Bertrand CASTELLI <b@brtcst.net>
2025-08-27 00:04:02 +0200

    modifications du jour

--- keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
+++ keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
@@ -41,7 +41,7 @@ enum custom_keycodes {
 
 enum tap_dance_codes {
   D_0,
-  D_1,
+  D_ESC_LOCK,
   D_2,
   D_3,
   D_4,
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     BP_CCED, BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM, TD(D_5),              XXXXXXX, BP_C,    BP_T,    BP_S,    BP_R,    BP_N,    BP_M,
     KC_LSFT, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,                                   BP_QUOT, BP_Q,    BP_G,    BP_H,    BP_F,    KC_RSFT,
     KC_LCTL, KC_LGUI, KC_LALT, KC_TAB,MO(3),         LGUI(BP_SCLN),       TD(D_2),          KC_BSPC, KC_DEL,  CMC_SLASH, MO(4),   KC_RCTL,
-                                        KC_SPC,  SH_MON,  TD(D_1),              TD(D_3), MO(2),   KC_RALT
+                                        KC_SPC,  SH_MON,  TD(D_ESC_LOCK),              TD(D_3), MO(2),   KC_RALT
   ),
   [GAMING] = LAYOUT_moonlander( //gaming
     _______, _______, _______, _______, _______, _______, BP_AT,                _______, _______, _______, _______, _______, _______, _______,
--- users/brtcst/tapdance.c
+++ users/brtcst/tapdance.c
@@ -25,11 +25,11 @@ void dance_custom_finished(tap_dance_state_t *state, void *user_data) {
     tap_dance_custom_t *dance = (tap_dance_custom_t *)user_data;
     dance->state = dance_step(state);
     switch (dance->state) {
-        case SINGLE_TAP: register_code(dance->single_tap); break;
-        case SINGLE_HOLD: register_code(dance->single_hold); break;
-        case DOUBLE_TAP: register_code(dance->double_tap); break;
-        case DOUBLE_HOLD: register_code(dance->double_hold); break;
-        case DOUBLE_SINGLE_TAP: tap_code(dance->single_tap); register_code(dance->single_tap);
+        case SINGLE_TAP: register_code16(dance->single_tap); break;
+        case SINGLE_HOLD: register_code16(dance->single_hold); break;
+        case DOUBLE_TAP: register_code16(dance->double_tap); break;
+        case DOUBLE_HOLD: register_code16(dance->double_hold); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(dance->single_tap); register_code16(dance->single_tap);
     }
 }
 
@@ -37,11 +37,11 @@ void dance_custom_reset(tap_dance_state_t *state, void *user_data) {
     tap_dance_custom_t *dance = (tap_dance_custom_t *)user_data;
     //wait_ms(10);
     switch (dance->state) {
-        case SINGLE_TAP: unregister_code(dance->single_tap); break;
-        case SINGLE_HOLD: unregister_code(dance->single_hold); break;
-        case DOUBLE_TAP: unregister_code(dance->double_tap); break;
-        case DOUBLE_HOLD: unregister_code(dance->double_hold); break;
-        case DOUBLE_SINGLE_TAP: unregister_code(dance->single_tap); break;
+        case SINGLE_TAP: unregister_code16(dance->single_tap); break;
+        case SINGLE_HOLD: unregister_code16(dance->single_hold); break;
+        case DOUBLE_TAP: unregister_code16(dance->double_tap); break;
+        case DOUBLE_HOLD: unregister_code16(dance->double_hold); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(dance->single_tap); break;
     }
     dance->state = NONE;
 }
