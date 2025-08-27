f8bd92f13a26242a57b06f7f96980b1ac15e74a1
Bertrand CASTELLI <bertrand.castelli@gmail.com>
2025-08-27 15:27:13 +0200

    quick tap

--- keyboards/zsa/moonlander/keymaps/brtcstLimited/config.h
+++ keyboards/zsa/moonlander/keymaps/brtcstLimited/config.h
@@ -37,7 +37,7 @@
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 120
 
 #define CHORDAL_HOLD
 // https://docs.qmk.fm/tap_hold#flow-tap
