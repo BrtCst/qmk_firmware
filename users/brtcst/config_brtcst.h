fffb6efbd96fd55b9ce8b640bcfc2ee6167d9c37
Bertrand CASTELLI <bertrand.castelli@gmail.com>
2025-09-16 13:24:30 +0200

    activation tapping term per key

--- users/brtcst/config_brtcst.h
+++ users/brtcst/config_brtcst.h
@@ -20,6 +20,8 @@
 
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 180
+#define TAPPING_TERM_PER_KEY
+
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 120
 
