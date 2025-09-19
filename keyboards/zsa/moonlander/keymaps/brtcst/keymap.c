605f28408401f04b97f634d40cdd8edfa7618417
Bertrand CASTELLI <b@brtcst.net>
2025-09-19 10:17:19 +0200

    exclusion espace du chordal hold

--- keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
+++ keyboards/zsa/moonlander/keymaps/brtcst/keymap.c
@@ -18,9 +18,9 @@ extern bool is_launching;
 
 
 char chordal_hold_handedness(keypos_t key) {
-    //if (key.col == 0 || key.col == MATRIX_COLS - 1) {
-    //    return '*';  // Exempt the outer columns.
-    //}
+    if (key.col == 0 || key.row == 5) { // espace
+        return '*';  // Exempt the outer columns.
+    }
     // On split keyboards, typically, the first half of the rows are on the
     // left, and the other half are on the right.
     return key.row < MATRIX_ROWS / 2 ? 'L' : 'R';
