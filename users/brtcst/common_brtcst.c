27c4f2e11193080b7d3dafcb5a876e5c813feae1
Bertrand CASTELLI <bertrand.castelli@gmail.com>
2025-09-26 12:19:07 +0200

    première version HRM ACSrA

--- users/brtcst/common_brtcst.c
+++ users/brtcst/common_brtcst.c
@@ -194,10 +194,16 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
                            uint16_t prev_keycode) {
     if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
     switch (get_tap_keycode(keycode)) {
+      // short term pour shift et ralt
+      case BP_I:
+      case BP_S:
+      case BP_I_MOD:
+      case BP_S_MOD:
       case BP_E:
       case BP_T:
       case BP_E_MOD:
       case BP_T_MOD:
+      case KC_SPC:
         return FLOW_TAP_TERM_SHORT;
       break;
       default:
@@ -210,8 +216,8 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 // Permissive hold
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case BP_I_MOD:
-        case BP_S_MOD:
+        case BP_U_MOD:
+        case BP_R_MOD:
             return false;
         default:
             return true;
