82e717799480f369227aad37f46bbf6ba4a6b77d
Bertrand CASTELLI <b@brtcst.net>
2026-06-11 15:59:53 +0200

    réduction term sur pouces

--- users/brtcst/config_brtcst.h
+++ users/brtcst/config_brtcst.h
@@ -22,8 +22,8 @@
 
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
-//#define TAPPING_TERM_SHORT 175
-//#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM_SHORT 175
+#define TAPPING_TERM_PER_KEY
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 120
